7b478f9768b47a008541dc02ef9b883d7eb36142
adultlee <seoungin1228@gmail.com>
2023-01-10 22:56:34 +0900

    feat: 초기 템플릿 작성

---
+++ components/Layout/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import styled from "@emotion/styled";
+import Text from "../Text";
+
+export const TopBarContainer = styled.header`
+	position: sticky;
+	height: 46px;
+	width: 100%;
+	backdrop-filter: saturate(180%) blur(20px);
+	background-color: black;
+
+	.container {
+		width: 100%;
+		max-width: 1140px;
+		margin: 0 auto;
+		padding: 0 22px;
+	}
+`;
+
+const HeaderText = styled(Text)`
+	line-height: 46px;
+`;
+
+const Header = () => {
+	return (
+		<TopBarContainer>
+			<div className="container">
+				<Link href="/">
+					<HeaderText color="white" size={20} weight={600}>
+						Blog
+					</HeaderText>
+				</Link>
+			</div>
+		</TopBarContainer>
+	);
+};
+
+export default Header;
