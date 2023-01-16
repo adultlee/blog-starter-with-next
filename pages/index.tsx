08c5f7691d6d836cd84e5fedaf31fcec807b63ac
adultlee <seoungin1228@gmail.com>
2023-01-16 13:47:40 +0900

    feat: 메인 페이지에서 postList에 props를 전달합니다

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,17 @@ import type { NextPage } from "next";
 import Layout from "../components/Layout/Layout";
 import PostList from "../components/PostList";
 import { allPosts, Post } from "../.contentlayer/generated";
-console.log(allPosts);
+
 export const getStaticProps = async () => {
 	const posts: Post[] = allPosts;
+
 	return { props: { posts } };
 };
 
 const Home = ({ posts }: { posts: Post[] }) => {
-	console.log(posts);
 	return (
 		<Layout>
-			<PostList />
+			<PostList posts={posts} />
 		</Layout>
 	);
 };
