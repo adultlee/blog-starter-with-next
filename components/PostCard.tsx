f10ab97066c35edfee65ffc0c1be8d2fa41478ae
adultlee <seoungin1228@gmail.com>
2023-01-20 16:01:57 +0900

    feat: postCard 기능추가
    author 의 프로필 ui에 대한 작업을 진행합니다

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,12 +3,13 @@ import styled from "@emotion/styled";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-
+import Text from "./Text";
 const PostCardWrapper = styled.div`
 	margin: 12px 0;
 	width: 300px;
 	min-height: 330px;
 	background-color: #170524;
+
 	@media (max-width: 750px) {
 		width: 100%;
 		min-height: 330px;
@@ -17,52 +18,7 @@ const PostCardWrapper = styled.div`
 	position: relative;
 	transition: 0.2s ease;
 	overflow: hidden;
-
-	.post-title {
-		display: inline-block;
-		position: relative;
-		font-size: 24px;
-		width: 100%;
-		overflow: hidden;
-		text-overflow: ellipsis;
-		color: #e0bcff;
-	}
-	.post-desc {
-		color: #e0bcff;
-		font-size: 18px;
-		margin-top: 5px;
-		line-height: 1.57;
-		opacity: 0.5;
-		width: auto;
-
-		/* 한 줄 자르기 */
-		display: inline-block;
-		width: 100%;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-
-		// 여러 줄 자르기 추가 스타일
-		white-space: normal;
-		line-height: 1.2;
-
-		text-align: left;
-		word-wrap: break-word;
-		display: -webkit-box;
-		-webkit-line-clamp: 5;
-		-webkit-box-orient: vertical;
-	}
-
-	.post-image {
-		width: 100%;
-		height: 150px;
-		overflow: hidden;
-		background-color: white;
-		img {
-			width: 100%;
-			height: 100%;
-		}
-	}
+	cursor: pointer;
 
 	&:hover {
 		transform: scale(1.1);
@@ -118,6 +74,72 @@ type PostCardProps = {
 	slug: string;
 };
 
+type ImageType = {
+	src: string;
+};
+
+const PostImage = styled.div`
+	width: 100%;
+	height: 150px;
+	overflow: hidden;
+	background-color: white;
+	img {
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+const PostTitle = styled.div`
+	display: inline-block;
+	position: relative;
+	font-size: 24px;
+	width: 100%;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	color: #e0bcff;
+	margin: 5px;
+`;
+
+const PostDescripption = styled.div`
+	color: #e0bcff;
+	font-size: 18px;
+	margin-top: 5px;
+	line-height: 1.57;
+	opacity: 0.5;
+	width: auto;
+	display: inline-block;
+	width: 100%;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: normal;
+	line-height: 1.2;
+	text-align: left;
+	word-wrap: break-word;
+	display: -webkit-box;
+	-webkit-line-clamp: 5;
+	-webkit-box-orient: vertical;
+	margin: 0px 5px;
+`;
+
+const PostProfileWrapper = styled.div`
+	display: flex;
+	margin: 20px 5px 3px;
+`;
+
+const PostProfileImage = styled.div<ImageType>`
+	border-radius: 50%;
+	width: 25px;
+	height: 25px;
+	display: flex;
+	margin-right: 10px;
+	border: 1px solid rgb(163, 151, 198);
+	background-size: contain;
+	background-image: url(${(props) => props.src});
+`;
+
+const PostProfileText = styled(Text)``;
+
 const PostCard = ({ title, description, thumbnail, slug }: PostCardProps) => {
 	const router = useRouter();
 
@@ -128,11 +150,19 @@ const PostCard = ({ title, description, thumbnail, slug }: PostCardProps) => {
 
 	return (
 		<PostCardWrapper onClick={handleClick}>
-			<div className="post-image">
+			<PostImage className="post-image">
 				<img src={thumbnail} />
-			</div>
-			<div className="post-title">{title}</div>
-			<div className="post-desc">{description}</div>
+			</PostImage>
+			<PostTitle className="post-title">{title}</PostTitle>
+			<PostDescripption className="post-desc">
+				{description}
+			</PostDescripption>
+			<PostProfileWrapper>
+				<PostProfileImage src={thumbnail} />
+				<PostProfileText color="#f9f1ff" size={16} weight={400}>
+					author
+				</PostProfileText>
+			</PostProfileWrapper>
 		</PostCardWrapper>
 	);
 };
